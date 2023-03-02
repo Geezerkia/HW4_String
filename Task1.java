92e24987fefd3776e6d7f709147f8d6ccd418571
Geezerkia <123498636+Geezerkia@users.noreply.github.com>
2023-03-03 01:00:36 +0200

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Task1 {
 
     //Створити метод findSymbolOccurrence.
@@ -7,46 +5,20 @@ public class Task1 {
     //Необхідно обчислити, скільки разів символ зустрічається в
     //переданому рядку і повернути це числове значення
 
-    static Scanner sc;
-    static String str;
-    static char symbol;
-    static char[] results;
-    static int counter;
-
 
     public static void main(String[] args) {
-        findSymbolOccurrence();
+
+        System.out.println("Number of occurrences is: " + findSymbolOccurrence("Apple", 'p'));
    }
 
-    public static void findSymbolOccurrence(){
-        doInput();
-        calculateOccurrences();
-        doOutput();
-    }
-
-    public static void doInput() {
-        sc = new Scanner(System.in);
-        System.out.println("This method calculates number of occurrences of a Symbol in a String.");
-        System.out.print("Please, enter a String: ");
-        str = sc.nextLine();
-        System.out.print("Please, enter a Symbol: ");
-        symbol = sc.next().charAt(0);
-        sc.close();
-    }
-
-public static void calculateOccurrences(){
-        results = str.toCharArray();
-    for (int i = 0; i < str.length(); i++) {
-        if (results[i] == symbol) {
-            counter++;}
-        }
-    }
-
-    public static void doOutput(){
-        if (counter == 0){
-            System.out.println("Number of symbol occurrences: " + 0);
-        } else {
-            System.out.println("Number of symbol occurrences: " + counter);
-        }
+    public static int findSymbolOccurrence(String str, char symbol) {
+        int counter = 0;
+        char[] results = str.toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+                if (results[i] == symbol) {
+                    counter++;
+                }
+            }
+        return counter;
     }
 }
