b32419505f1e5758504f3c6b2d419826db63d7b5
Geezerkia <123498636+Geezerkia@users.noreply.github.com>
2023-03-05 13:40:31 +0200

    Update Task3.java

--- Task3.java
+++ Task3.java
@@ -5,15 +5,18 @@ public class Task3 {
     // Наприклад:
     // Hello -> olleH
 
-
     public static void main(String[] args) {
-
         System.out.println("The original string is: " + "Apple" +
                 "\nThe reversed String is: " + stringReverse("Apple"));
     }
-
     public static String stringReverse(String str) {
-
+        strValidator(str);
         return new StringBuilder(str).reverse().toString();
+    }
+    static void strValidator(String strVal) {
+        if (strVal == null) {
+            System.out.println("You entered a Null String, please, correct.");
+            System.exit(0);
+        }
     }
 }
