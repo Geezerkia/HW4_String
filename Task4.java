2cd64854c90da9f039b3b075a0567b3a6a7f86b8
Geezerkia <123498636+Geezerkia@users.noreply.github.com>
2023-03-05 13:42:14 +0200

    Update Task4.java

--- Task4.java
+++ Task4.java
@@ -11,13 +11,17 @@ public class Task4 {
     // Allo -> false
 
     public static void main(String[] args) {
-
         System.out.println("The string " + "'kayak'" + " is Palindrome: " + isPalindrome("kayak"));
     }
-
     public static boolean isPalindrome(String str){
-
+        strValidator(str);
         String strReversed = new StringBuilder(str).reverse().toString();
         return str.equals(strReversed);
+    }
+    static void strValidator(String strVal) {
+        if (strVal == null) {
+            System.out.println("You entered a Null String, please, correct.");
+            System.exit(0);
+        }
     }
 }
